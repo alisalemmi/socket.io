4b1e65c84189706e1629977281f51fc09a81aa93
ali salemi <alisalemmi@outlook.com>
2021-02-23 10:35:27 +0330

    room طراحی scrollbar طراحی

--- 2 namespase and room/frontend/src/views/Chat.vue
+++ 2 namespase and room/frontend/src/views/Chat.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
 .chat
-  ul.chat__room__box
+  ul.chat__room
+    li.chat__room__box(v-for='(room, index) in rooms', :key='index')
+      img.chat__room__image(
+        src='https://www.gardenia.net/storage/app/public/guides/detail/UMiTBTJea8fOlL0ca396xU75ZmnS6HE4HqtkVJUN.jpeg'
+      )
+      h4.chat__room__title محمد
+      span.chat__room__date 14:56
+      p.chat__room__last خوبی؟ حالت چطوره؟
 
   form.chat__send
     #chat__send__message.chat__send__message(
@@ -29,7 +36,8 @@ export default {
   name: 'Chat',
   data: function () {
     return {
-      message: ''
+      message: '',
+      rooms: [{}, {}, {}, {}, {}, {}, {}, {}, {}]
     };
   },
   methods: {
@@ -55,14 +63,80 @@ export default {
 
   height: 100%;
 
-  background-color: $color-light-green;
-
   @include no-drag();
 
   &__room {
+    $padding-tb: 2rem;
+    $padding-lr: 1.5rem;
+    $scroll-width: 0.75rem;
+
+    grid-area: rooms;
+
+    background-color: $background-white-2;
+    list-style: none;
+    direction: ltr; // go scroll bar right
+
+    @include scrollbar($width: $scroll-width);
+
     &__box {
-      grid-area: rooms;
-      background-color: $color-red;
+      display: grid;
+      grid-template-columns: 5rem 1fr max-content;
+      grid-template-areas:
+        'image title date'
+        'image last last';
+      grid-gap: 0.5rem 1.5rem;
+
+      direction: rtl; // go scroll bar right
+      padding: $padding-tb $padding-lr;
+      padding-right: max($padding-lr - $scroll-width, 0);
+
+      cursor: pointer;
+      transition: all 0.3s ease;
+
+      &:hover {
+        background-color: darken($color: $background-white, $amount: 8);
+      }
+    }
+
+    &__image {
+      display: block;
+      width: 5rem;
+      height: 5rem;
+      grid-area: image;
+      align-self: center;
+
+      border-radius: 50%;
+    }
+
+    &__title {
+      width: 100%;
+      grid-area: title;
+
+      font-weight: 400;
+      font-size: 1.7rem;
+      text-align: right;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    &__date {
+      grid-area: date;
+      justify-self: left;
+
+      font-size: 1.3rem;
+      color: $color-text-gray;
+    }
+
+    &__last {
+      width: 100%;
+      grid-area: last;
+
+      font-size: 1.3rem;
+      text-align: right;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
     }
   }
 
@@ -81,6 +155,7 @@ export default {
       padding: $padding-message-tb $padding-message-lr $padding-message-tb
         $padding-message-lr + $send-icon-size;
       width: 100%;
+      max-height: 25vh;
 
       font-size: $font-size;
       line-height: $line-height;
@@ -88,13 +163,15 @@ export default {
       outline: none;
       border: none;
       cursor: text;
+      overflow-y: auto;
 
       user-select: text;
       white-space: pre-wrap;
       overflow-wrap: break-word;
       -webkit-user-modify: read-write-plaintext-only;
 
-      @include round-box();
+      @include round-box($background: #f1f1f1, $box-shadow: none);
+      @include scrollbar();
     }
 
     &__label {
